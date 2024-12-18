8e3a14620b3684fcec0cb39a1d3f81d59307262c
JWALITHA11 <jwalithamarella@gmail.com>
2024-12-18 20:11:13 +0530

    changes made

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Home: React.FC = () =>{
     return (
     <div className='services-container'>
-        <h1>Welcome to Home Page</h1>
+        <h1>Welcome to Home Page!!</h1>
         </div>
     );
 };
